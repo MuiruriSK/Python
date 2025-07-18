a97c5098a396ac372481539fe00a48cafb16c314
MuiruriSK <muirurisolomon602@gmail.com>
2025-07-18 15:57:43 +0300

    Initial commit

---
+++ Day 4/nested_lists.py
@@ -0,0 +1,16 @@
+# dirty_dozen = [Strawberries, Spinach, Kale, Tomatoes, Celery, Cucumber, Potatoes, Sweet Potatoes, Bell Peppers, Lettuce, Apples, Grapes
+# , Peaches, Nectarines, Cherries, Pears, Plums, Raspberries, Blueberries, Blackberries]
+# This is a list of the dirty dozen fruits and vegetables
+# The dirty dozen is a list of fruits and vegetables that are known to have high pesticide residues
+# The list is updated annually by the Environmental Working Group (EWG)
+# The EWG is a non-profit organization that works to protect public health and the environment
+# The list is based on data from the US Department of Agriculture (USDA)
+fruits =["Strawberries", "Tomatoes", "Celery", "Cucumber", "Apples",
+          "Grapes", "Peaches", "Nectarines", "Cherries", "Pears", "Plums", "Raspberries", "Blueberries", "Blackberries"]
+vegetables = ["Spinach", "Kale", "Potatoes", "Sweet Potatoes", "Bell Peppers", "Lettuce"]
+dirty_dozen = [fruits , vegetables]
+print (dirty_dozen)  # Printing the dirty dozen list
+print (dirty_dozen[0])  # Printing the fruits list
+print (dirty_dozen[1])  # Printing the vegetables list
+print (dirty_dozen[0][0])  # Printing the first fruit
+print (dirty_dozen[1][0])  # Printing the first vegetable
