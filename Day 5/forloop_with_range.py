e34c0d92a63294665388ae73da210cc0ace4e9ad
MuiruriSK <muirurisolomon602@gmail.com>
2025-07-21 13:04:39 +0300

    Initial commit

---
+++ Day 5/forloop_with_range.py
@@ -0,0 +1,20 @@
+for number in range(1, 10):
+    print(number)  # Printing numbers from 1 to 9
+    print(number * 2)  # Printing double of each number
+
+# We can specify a step in the range function
+for number in range(1, 10, 2):
+    print(number)  # Printing odd numbers from 1 to 9
+    print(number * 2)  # Printing double of each odd number
+
+# Using range to iterate through a list
+numbers = [10, 20, 30, 40, 50]
+for index in range(len(numbers)):
+    print(f"Index: {index}, Value: {numbers[index]}")  # Printing index and value of each element in the list
+    print(numbers[index] + 5)  # Adding 5 to each number in the list
+
+# Adding numbers from 1 to 100 using a for loop
+total = 0
+for number in range(1, 101):
+    total += number  # Summing numbers from 1 to 100
+print(f"Total sum from 1 to 100 is: {total}")  # Printing
