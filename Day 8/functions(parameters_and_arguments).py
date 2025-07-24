88857871933523b6a8bf3eb07c945fe7ce54b809
MuiruriSK <muirurisolomon602@gmail.com>
2025-07-24 10:56:44 +0300

    Positional arguments

---
+++ Day 8/functions(parameters_and_arguments).py
@@ -0,0 +1,20 @@
+# def greet():
+#     print("Hello!")
+#     print("How are you today?")
+#     print("It's nice to meet you!")
+
+# greet()
+
+# # Functions that allow for parameters and arguments
+# def greet_with_name(name):
+#     print(f"Hello {name}!")
+#     print(f"How do you do {name}?")
+
+# greet_with_name("Solomon")
+
+# Functions with more than one parameter
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
+
+greet_with("Solomon", "London")
