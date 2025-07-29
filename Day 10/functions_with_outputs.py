ad40ab478c155cf9cc82cb3e2798e18ffc7e88cf
MuiruriSK <muirurisolomon602@gmail.com>
2025-07-29 11:55:01 +0300

    Slight changes.

--- Day 10/functions_with_outputs.py
+++ Day 10/functions_with_outputs.py
@@ -1,8 +1,10 @@
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+    return f"Result: {formated_f_name} {formated_l_name}"
 
-formated_string = format_name("SOLOMON", "MuIRuRi")
-print(formated_string)  # Output: Solomon Muiruri
+# formated_string = format_name("SOLOMON", "MuIRuRi")
+print(format_name(input("What is your first name?"), input("What is your last name?")))  # Output: Solomon Muiruri
     
