38af6c09b6628e2de3a5e56dd27c013b7c7341b4
MuiruriSK <muirurisolomon602@gmail.com>
2025-08-05 07:47:00 +0300

    Defining more blackjack rules.

--- Day 11/Blackjack_game.py
+++ Day 11/Blackjack_game.py
@@ -6,14 +6,27 @@ def deal_card():
     card = random.choice(cards)
     return card
 
+def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards."""
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-def calculate_score(cards):
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
-    return sum(cards)
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f" Your cards: {user_cards}, current score: {user_score}")
+print(f" Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
